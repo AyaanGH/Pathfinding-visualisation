1dcc6507ccfb6f09cc77dd9d95edcb6efcdfe3cf
AyaanGH <{ID}+{username}@users.noreply.github.com>
2019-12-17 22:54:37 +0000

    create start and end point

--- pathSolver.py
+++ pathSolver.py
@@ -5,6 +5,8 @@ Generate maze to solve
 """
 
 
+import random
+
 def setSize():
     size_x = int(input('X size\n'))
     size_y = int(input('Y size\n'))
@@ -31,12 +33,36 @@ def print_table(table):
        print(' '.join(row))
 
 
+def startPoint(table,xLen,yLen):
+    start_x = random.randint(0,xLen-1)
+    start_y = random.randint(0,yLen-1)
+
+    print(start_x,start_y)
+
+    table[start_y][start_x] = 'O'
+
+    while True :
+        end_x = random.randint(0, xLen-1)
+        end_y = random.randint(0, yLen-1)
+
+        if end_x == start_x  or end_y == start_y:
+            continue
+
+        table[end_y][end_x ] = 'X'
+        
+        break 
+    print(end_x,end_y)
+
+    return table
 
 
 if __name__ == "__main__":
     x,y = setSize()
-    table = construct_table(x,y)
-    print_table(table)
+    empty_table = construct_table(x,y)
+    print_table(empty_table)
+    start_point_table = startPoint(empty_table,x,y)
+
+    print_table(start_point_table)
 
 """
  . . . . . . . . . . . . . . . . .
