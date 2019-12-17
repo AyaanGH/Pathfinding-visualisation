7126c1744ff8153ba504087549a478930b2a4ced
AyaanGH <{ID}+{username}@users.noreply.github.com>
2019-12-17 22:24:14 +0000

    Print table defined by user

--- pathSolver.py
+++ pathSolver.py
@@ -3,3 +3,60 @@
 Generate maze to solve
 
 """
+
+
+def setSize():
+    size_x = int(input('X size\n'))
+    size_y = int(input('Y size\n'))
+
+    return size_x,size_y
+
+def construct_table(size_x,size_y):
+
+    table = []
+
+    
+    for i in range(size_y):
+        row = []
+        for k in range(size_x):
+            row.append(".")
+        table.append(row)
+    
+    return table
+
+
+def print_table(table):
+
+    for row in table:
+       print(' '.join(row))
+
+
+
+
+if __name__ == "__main__":
+    x,y = setSize()
+    table = construct_table(x,y)
+    print_table(table)
+
+"""
+ . . . . . . . . . . . . . . . . .
+ . . . . . . . . . . . # . . . . .
+ . . . . . . . . . . . # - B . . .
+ . . . . . . . . . . . # . . . . .
+ . . . . . . . . . . . # . . . . .
+ . . A ----------------# . . . . .
+ . . . . . . . . . . . . # . . . .
+ . . . . . . . . . . . . . . . . .
+
+
+
+|
+|
+|
+|
+|
+|
+
+
+
+"""
