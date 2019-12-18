15ef7afbcffdc1079f856cfa5877f77cab5168ab
AyaanGH <{ID}+{username}@users.noreply.github.com>
2019-12-18 13:19:33 +0000

    obstacle generated

--- pathSolver.py
+++ pathSolver.py
@@ -6,6 +6,7 @@ Generate maze to solve
 
 
 import random
+import copy
 
 def setSize():
     size_x = int(input('X size\n'))
@@ -55,14 +56,50 @@ def startPoint(table,xLen,yLen):
 
     return table
 
+def obstacle(table,x,y):
+    obstacle_quantity = int(0.1*x*y)
+    print(obstacle_quantity,"obstacle amount")
+
+    obstacles_generated = 0
+    obstacle_y = 0
+    obstacle_x = 0
+   
+
+    while True:
+        obstacle_x = random.randint(0, x - 1)
+        obstacle_y = random.randint(0, y - 1)
+        coordinate = table[obstacle_y][obstacle_x]
+        print(obstacles_generated)
+        print(coordinate,obstacle_x,obstacle_y)
+
+        if obstacle_quantity == obstacles_generated:
+            break
+
+        elif  coordinate == ".":
+             table[obstacle_y][obstacle_x] = "#"
+             obstacles_generated += 1 
+    
+    return table
+
+
+
 
 if __name__ == "__main__":
     x,y = setSize()
     empty_table = construct_table(x,y)
-    print_table(empty_table)
+    # print_table(empty_table)
     start_point_table = startPoint(empty_table,x,y)
 
     print_table(start_point_table)
+    obstacle_table = obstacle(start_point_table,x , y)
+
+    print_table(obstacle_table)
+
+    # while True:
+    #     temp = input("")
+    #     fresh_obstacle_table = copy.deepcopy(start_point_table)
+    #     test = obstacle(fresh_obstacle_table, x, y)
+    #     print_table(test)
 
 """
  . . . . . . . . . . . . . . . . .
@@ -70,7 +107,7 @@ if __name__ == "__main__":
  . . . . . . . . . . . # - B . . .
  . . . . . . . . . . . # . . . . .
  . . . . . . . . . . . # . . . . .
- . . A ----------------# . . . . .
+ . . A - - - - - - - - # . . . . .
  . . . . . . . . . . . . # . . . .
  . . . . . . . . . . . . . . . . .
 
