4f25769e7e4427c553bc50abcdf5687e83528d6a
AyaanGH <{ID}+{username}@users.noreply.github.com>
2020-01-03 16:41:45 +0000

    BFS working

--- pathSolver.py
+++ pathSolver.py
@@ -4,11 +4,6 @@ Generate maze to solve
 
 
 
-
-
-
-
-
 """
 
 
@@ -66,7 +61,8 @@ def startPoint(table,xLen,yLen):
     return table,start_x,start_y,end_x,end_y
 
 def obstacle(table,x,y):
-    obstacle_quantity = int(0.2*x*y)
+    percent_obs = 0.2
+    obstacle_quantity = int(percent_obs*x*y)
     # print(obstacle_quantity,"obstacle amount")
 
     obstacles_generated = 0
@@ -80,6 +76,8 @@ def obstacle(table,x,y):
         coordinate = table[obstacle_y][obstacle_x]
         # print(obstacles_generated)
         # print(coordinate,obstacle_x,obstacle_y)
+
+        
 
         if obstacle_quantity == obstacles_generated:
             break
@@ -105,7 +103,7 @@ def genCoordinates(table,start_x,start_y,end_x,end_y):
 
     
 
-def dijikstra(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,size_y):
+def weighted_search(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,size_y):
      list_of_coordinates = copy.deepcopy(coords)
      stack = []
      visited = []
@@ -235,6 +233,9 @@ def dijikstra(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,size_y):
             print("Fixing")
             table[y_val][x_val] = "#"
 
+            if len(visited) > size_x * size_y +1:
+                return False
+
             del visited[-1]
             current_visiting_node = visited[-1]
 
@@ -245,6 +246,8 @@ def dijikstra(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,size_y):
             x_val = current_visiting_node[0]
             y_val = current_visiting_node[1]
             table[y_val][x_val] = "."
+
+            
 
 
 
@@ -280,6 +283,209 @@ def genNodeMap(table,end_x,end_y):
 
 
 
+def foundEnd(coord,end_x,end_y):
+
+    print("Debug, printing Coord",coord)
+    k = coord[-1][0]
+    i = coord[-1][1]
+    end_distance = math.sqrt(((end_x - k)**2 + (end_y - i)**2))
+
+    print(end_distance)
+
+    if end_distance == 1 or end_distance == 1.0:
+        return True
+    
+    else:
+        return False
+
+
+
+def check_valid_north(table,coordinate,size_x,size_y):
+
+    x_val = coordinate[0]
+    y_val = coordinate[1] - 1
+
+    try:
+            if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
+                if y_val >= 0:
+                     print('North is a valid node')
+
+                     return [x_val, y_val]
+
+                else:
+                    print('North is NOT a valid node')
+
+                    return False
+
+    except IndexError:
+            print("North is out of range")
+            return False
+
+
+def check_valid_south(table, coordinate, size_x, size_y):
+    x_val = coordinate[0]
+    y_val = coordinate[1] + 1
+
+    try:
+            if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
+                if y_val <= size_y-1:
+                     print('South is a valid node')
+
+                     return [x_val, y_val]
+
+                else:
+                    print('South is NOT a valid node')
+
+                    return False
+
+    except IndexError:
+            print("South is out of range")
+            return False
+    
+
+def check_valid_east(table, coordinate, size_x, size_y):
+    x_val = coordinate[0]+1
+    y_val = coordinate[1] 
+
+    try:
+            if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
+                if x_val <= size_x - 1:
+                     print('east is a valid node')
+
+                     return [x_val, y_val]
+
+                else:
+                    print('east is NOT a valid node')
+
+                    return False
+
+    except IndexError:
+            print("east is out of range")
+
+            return False
+
+
+def check_valid_west(table, coordinate, size_x, size_y):
+    x_val = coordinate[0] -1
+    y_val = coordinate[1]
+
+    try:
+            if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
+                if x_val >= 0:
+                     print('west is a valid node')
+
+                     return [x_val, y_val]
+
+                else:
+                    print('west is NOT a valid node')
+
+                    return False
+
+    except IndexError:
+            print("west is out of range")
+            return False
+
+
+
+def valid(action,table,deque,size_x,size_y):
+
+
+
+    if action == "N":
+        return check_valid_north(table,deque,size_x,size_y)
+
+    elif action == "S":
+        return check_valid_south(table, deque, size_x, size_y)
+    
+    elif action == "E":
+        return check_valid_east(table, deque, size_x, size_y)
+    
+    elif action == "W":
+        return check_valid_west(table, deque, size_x, size_y)
+
+    
+    
+
+
+   
+
+
+
+
+def bfs(coords,table, start_x,start_y,end_x,end_y,size_x,size_y):
+
+    table = copy.deepcopy(table)
+
+    print_table(table)
+    input("")
+    queue = []
+    action_list = []
+    queue.append([[start_x,start_y]])
+    dequeued = queue[0]
+
+    
+
+    
+    
+    #last_queue_coord = queue[0][-1]
+
+    while foundEnd(dequeued,end_x,end_y) is False:
+        print("Queue: ", queue)
+        print("Distance to end", foundEnd(dequeued, end_x, end_y))
+        dequeued = queue[0]
+
+
+        #TODO when on second round, it keeps checking first value of queue
+
+        print("Dequed Sequence: ", dequeued)
+        del queue[0]
+        print("Queue remaining: ", queue)
+
+        last_coord_of_dequed = dequeued[-1]
+
+        for actions in ["N","S","E","W"]:
+            print_table(table)
+
+           # print("in for loop, printing dequed and then 'dequed[-1]' ",dequeued)
+           # print(dequeued[-1])
+
+            check_actions = valid(actions, table, last_coord_of_dequed,size_x,size_y)
+
+            #TODO check logic of adding new value to dequed or queue
+
+            if check_actions is False or check_actions is None:
+                print(actions,"is not viable")
+                continue
+
+            dequeued_copy = copy.deepcopy(dequeued)
+            dequeued_copy.append(check_actions)
+
+            #print(add_to_dequeued)
+
+            #TODO problem appears to be here
+            queue.append(dequeued_copy)
+            print("The Queue", queue)
+
+            print("printing check actions ", check_actions)
+
+            table[check_actions[1]][check_actions[0]] = "#"
+    
+    print("FOUND END")
+    print(dequeued)
+
+    return dequeued
+
+
+            #if  is True:
+
+            
+
+
+
+    
+
+
+
 
 def path_table(visits,obstacle_table):
     print(visits)
@@ -295,47 +501,61 @@ def path_table(visits,obstacle_table):
 
 
 if __name__ == "__main__":
-    x,y = setSize()
-    empty_table = construct_table(x,y)
-    # print_table(empty_table)
-    start_point_table, start_x,start_y,end_x,end_y = startPoint(empty_table,x,y)
+    while True:
+        x,y = setSize()
+        empty_table = construct_table(x,y)
+        # print_table(empty_table)
+        start_point_table, start_x,start_y,end_x,end_y = startPoint(empty_table,x,y)
 
-    #print_table(start_point_table)
-    obstacle_table = obstacle(start_point_table,x , y)
+        #print_table(start_point_table)
+        obstacle_table = obstacle(start_point_table,x , y)
 
-    print_table(obstacle_table)
-    coords = genCoordinates(obstacle_table,start_x,start_y,end_x,end_y)
-    node_map = genNodeMap(obstacle_table,end_x,end_y)
-    visits = dijikstra(obstacle_table,coords,start_x,start_y,end_x,end_y,node_map,x,y)
+        print_table(obstacle_table)
 
-    print("""
-    
-    
+        coords = genCoordinates(obstacle_table,start_x,start_y,end_x,end_y)
+        node_map = genNodeMap(obstacle_table,end_x,end_y)
 
+        weighted_search_result_table = copy.deepcopy(obstacle_table)
+        weighted_search_result = weighted_search(weighted_search_result_table, coords, start_x, start_y, end_x, end_y, node_map, x, y)
 
+        if weighted_search_result is False:
+            print('\nNo Solution')
+            continue
 
-
-    
-
-
-
-
-
+        print("""
+        
+        ======================Weighted Search=============================
 
 
 
+        """)
+
+        print_table( path_table(weighted_search_result,weighted_search_result_table))
+
+        print(weighted_search_result)
+        print("Distance is ", len(weighted_search_result))
+
+
+        cont = input("")
+
+
+        print("==============BREATH FIRST SEARCH==========")
+
+        bfs_table = copy.deepcopy(obstacle_table)
+
+        bfs_result = bfs(coords,bfs_table,start_x,start_y,end_x,end_y,x,y)
+
+        bfs_result = bfs_result[1:]
 
 
 
+        print_table(path_table(bfs_result, bfs_table))
+        print(bfs_result)
+        print("Distance is ", len(bfs_result))
 
 
-
-
-    """)
-
-    print_table( path_table(visits,obstacle_table))
-
-    print(visits)
+   
+        
 
     #print(coords)
 
