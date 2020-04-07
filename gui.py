ff5a0d24212ad59b90cf71192373a72aa4337322
AyaanGH <25395404+AyaanGH@users.noreply.github.com>
2020-04-07 19:15:53 +0100

    Weighted Search semi working

--- gui.py
+++ gui.py
@@ -1,26 +1,10 @@
 import tkinter as tk
 import random
+import json
+import math
 
 
-root = tk.Tk()
-sidebar = tk.Frame(root, width=200, height=500, borderwidth=2)
 
-start_button  = tk.Button(sidebar, text = "Start", width = 200//5)
-stop_button  = tk.Button(sidebar, text = "Stop", width = 200//5)
-clear_button  = tk.Button(sidebar, text = "Clear", width = 200//5)
-
-
-clicked = tk.StringVar()
-clicked.set("Pathfinding Algorithm")
-drop_down = tk.OptionMenu(sidebar, clicked, "Weighted Search", "Breath First Search")
-
-    
-start_button.grid(row = 0)
-stop_button.grid(row = 1)
-clear_button.grid(row = 2)
-
-drop_down.grid(row=3)
-sidebar.pack(expand=False, fill='both', side='right', anchor='nw')
 class Grid:
 
     def __init__(self, width, height, square_size):
@@ -38,9 +22,12 @@ class Grid:
 
             self.grid.append(temp)
 
-    def start(self):
+    def update_cell(self):
+
+        
         for i in range(len(self.grid)):
             for k in range(len(self.grid[i])):
+        
                 if self.grid[i][k].get_colour() == "green":
                     self.start_cell = self.grid[i][k]
                 
@@ -54,6 +41,10 @@ class Cell:
 
     def __init__(self, canvas, x, y, size):
         self.canvas = canvas
+        self.x = int(x /size)
+        self.y = int(y /size)
+
+        self.viable = True
         self.box = self.canvas.create_rectangle(x, y, x+size, y+size, fill="white")
         # self.canvas.tag_bind(self.box,"<Motion><B1-Motion>",lambda x: self.set_colour("pink"))
         # self.colour = ""
@@ -106,6 +97,8 @@ class Cell:
             elif green_set == False and red_set == True:
                 self.set_colour("green")
                 myGrid.green_set = True
+        
+        myGrid.update_cell()
 
 
     def on_click(self, event):
@@ -124,6 +117,167 @@ class Cell:
     def on_release(self, event):
         self.dragging = False
 
+
+
+def init_weighted_search():
+
+
+    #Check NESW for cell closest to end
+    # root.after()
+    size_y = len(myGrid.grid)
+    size_x = len(myGrid.grid[0])
+    holding = []
+    visited = []
+    visited.append([myGrid.start_cell.x,myGrid.start_cell.y])
+    
+    def weighted_search():
+
+        print(myGrid.start_cell.x, myGrid.start_cell.y)
+        current_visiting_node = visited[-1]
+        # Check north
+
+        x_val = current_visiting_node[0]
+        y_val = current_visiting_node[1]-1
+
+        # print("checking North:")
+        # print("Current Visitng node",x_val,",",y_val," " , visited)
+
+        try:
+            if myGrid.grid[y_val][x_val].get_colour() == "white" or myGrid.grid[y_val][x_val].get_colour() == "red":
+                if y_val >= 0:
+                    print('North is a valid node')
+                    holding.append([x_val, y_val])
+
+        except IndexError:
+            pass
+
+        x_val = current_visiting_node[0]
+        y_val = current_visiting_node[1] + 1
+
+
+        try:
+            if myGrid.grid[y_val][x_val].get_colour() == "white" or myGrid.grid[y_val][x_val].get_colour() == "red":
+                if y_val <= size_y-1:
+                    # print('South is a valid node')
+                    holding.append([x_val, y_val])
+
+        except IndexError:
+            pass
+            # print("South is out of range")
+        # Check East
+
+        x_val = current_visiting_node[0]+1
+        y_val = current_visiting_node[1]
+
+        # print("checking East:")
+        # print("Current Visitng node", x_val, ",", y_val, " ", visited)
+
+        try:
+            if myGrid.grid[y_val][x_val].get_colour() == "white" or myGrid.grid[y_val][x_val].get_colour() == "red":
+                if x_val <= size_x-1:
+                    # print('East is a valid node')
+                    holding.append([x_val, y_val])
+
+        except IndexError:
+            # print("East is out of range")
+            pass
+
+        # Check WEst
+
+        x_val = current_visiting_node[0] - 1
+        y_val = current_visiting_node[1]
+
+        # print("checking West:")
+        # print("Current Visitng node", x_val, ",", y_val, " ", visited)
+
+        try:
+            if myGrid.grid[y_val][x_val].get_colour() == "white" or myGrid.grid[y_val][x_val].get_colour() == "red":
+                if x_val >= 0:
+                    # print('West is a valid node')
+                    # print("symbol at west is",myGrid.grid[y_val][x_val])
+                    holding.append([x_val, y_val])
+
+        except IndexError:
+            pass
+            # print("West is out of range")
+
+        if holding != []:
+            print("We have found the following visitable nodes around",current_visiting_node)
+            print("Visitable nodes",holding)
+
+            weight_map = {}
+
+            for node in holding:
+                # print(node)
+                weight_map[str(node)] = math.sqrt(((myGrid.end_cell.x - node[0])**2 + (myGrid.end_cell.y - node[1])**2))
+
+                # print('weight map')
+                print(weight_map)
+
+                # Use dictonary to find ideal weight
+
+            minVal = (min(weight_map, key=weight_map.get))
+
+            if weight_map[minVal] == 0.0:
+                # print("shortest found")
+                del visited[0]
+                return visited
+
+            # print("best node to visit is ", minVal)
+
+            clean_list = json.loads(minVal)
+            clean_list_x = clean_list[0]
+            clean_list_y = clean_list[1]
+            myGrid.grid[clean_list_y][clean_list_x].set_colour('blue')
+
+            visited.append(clean_list)
+
+        else:
+            print("There are no visitable nodes ")
+            print("Fixing")
+            myGrid.grid[y_val][x_val].set_colour('blue')
+
+            if len(visited) > size_x * size_y + 1:
+                return False
+
+            del visited[-1]
+            current_visiting_node = visited[-1]
+
+            # Check north
+
+            x_val = current_visiting_node[0]
+            y_val = current_visiting_node[1]
+            myGrid.grid[y_val][x_val].set_colour('white')
+
+
+        # myGrid.grid[myGrid.start_cell.x][myGrid.start_cell.y] 
+
+
+
+        root.after(100,weighted_search)
+    weighted_search()
+
+
+
+root = tk.Tk()
+sidebar = tk.Frame(root, width=200, height=500, borderwidth=2)
+
+start_button  = tk.Button(sidebar, text = "Start", width = 200//5,command = init_weighted_search )
+stop_button  = tk.Button(sidebar, text = "Stop", width = 200//5)
+clear_button  = tk.Button(sidebar, text = "Clear", width = 200//5)
+
+
+clicked = tk.StringVar()
+clicked.set("Pathfinding Algorithm")
+drop_down = tk.OptionMenu(sidebar, clicked, "Weighted Search", "Breath First Search")
+
+
+start_button.grid(row = 0)
+stop_button.grid(row = 1)
+clear_button.grid(row = 2)
+
+drop_down.grid(row=3)
+sidebar.pack(expand=False, fill='both', side='right', anchor='nw')
 
 # root.resizable(False,False)
 w = tk.Canvas(root, width=500, height=500)
@@ -144,9 +298,10 @@ myGrid = Grid(width, height, square_size)
 
 
 
-myGrid.start()
+# myGrid.start()
 
 w.pack()
+
 
 
 root.mainloop()
--- pathSolver.py
+++ pathSolver.py
@@ -14,55 +14,57 @@ import json
 import time
 import os
 
+
 def setSize():
     size_x = int(input('X size\n'))
     size_y = int(input('Y size\n'))
 
-    return size_x,size_y
+    return size_x, size_y
 
-def construct_table(size_x,size_y):
+
+def construct_table(size_x, size_y):
 
     table = []
 
-    
     for i in range(size_y):
         row = []
         for k in range(size_x):
             row.append(".")
         table.append(row)
-    
+
     return table
 
 
 def print_table(table):
 
     for row in table:
-       print(' '.join(row))
+        print(' '.join(row))
 
 
-def startPoint(table,xLen,yLen):
-    start_x = random.randint(0,xLen-1)
-    start_y = random.randint(0,yLen-1)
+def startPoint(table, xLen, yLen):
+    start_x = random.randint(0, xLen-1)
+    start_y = random.randint(0, yLen-1)
 
     # print(start_x,start_y)
 
     table[start_y][start_x] = 'O'
 
-    while True :
+    while True:
         end_x = random.randint(0, xLen-1)
         end_y = random.randint(0, yLen-1)
 
-        if end_x == start_x  or end_y == start_y:
+        if end_x == start_x or end_y == start_y:
             continue
 
-        table[end_y][end_x ] = 'X'
-        
-        break 
+        table[end_y][end_x] = 'X'
+
+        break
     # print(end_x,end_y)
 
-    return table,start_x,start_y,end_x,end_y
+    return table, start_x, start_y, end_x, end_y
 
-def obstacle(table,x,y):
+
+def obstacle(table, x, y):
     percent_obs = 0.2
     obstacle_quantity = int(percent_obs*x*y)
     # print(obstacle_quantity,"obstacle amount")
@@ -70,7 +72,6 @@ def obstacle(table,x,y):
     obstacles_generated = 0
     obstacle_y = 0
     obstacle_x = 0
-   
 
     while True:
         obstacle_x = random.randint(0, x - 1)
@@ -79,49 +80,46 @@ def obstacle(table,x,y):
         # print(obstacles_generated)
         # print(coordinate,obstacle_x,obstacle_y)
 
-        
-
         if obstacle_quantity == obstacles_generated:
             break
 
-        elif  coordinate == ".":
-             table[obstacle_y][obstacle_x] = "#"
-             obstacles_generated += 1 
-    
+        elif coordinate == ".":
+            table[obstacle_y][obstacle_x] = "#"
+            obstacles_generated += 1
+
     return table
 
-def genCoordinates(table,start_x,start_y,end_x,end_y):
+
+def genCoordinates(table, start_x, start_y, end_x, end_y):
 
     coordinates = []
 
     for i in range(len(table)):
         for k in range(len(table[i])):
-            weights =  math.sqrt(((end_x - i)**2 + (end_y - k)**2))
-            
-    
-            coordinates.append([i,k,round(weights,4)])
+            weights = math.sqrt(((end_x - i)**2 + (end_y - k)**2))
+
+            coordinates.append([i, k, round(weights, 4)])
 
     return coordinates
 
-    
 
-def weighted_search(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,size_y):
-     list_of_coordinates = copy.deepcopy(coords)
-     stack = []
-     visited = []
-     stack.append([start_x,start_y])
-     visited.append([start_x,start_y])
-     distance_counter = 0
+def weighted_search(table, coords, start_x, start_y, end_x, end_y, node_map, size_x, size_y):
+    list_of_coordinates = copy.deepcopy(coords)
+    stack = []
+    visited = []
+    stack.append([start_x, start_y])
+    visited.append([start_x, start_y])
+    distance_counter = 0
 
-     while True:
+    while True:
 
         holding = []
-        
+
         current_visiting_node = visited[-1]
 
         # print('cURRENT VISINT NODE: ', current_visiting_node)
 
-        #Check north
+        # Check north
 
         x_val = current_visiting_node[0]
         y_val = current_visiting_node[1]-1
@@ -131,16 +129,14 @@ def weighted_search(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,siz
 
         try:
             if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
-                if y_val >=0:
+                if y_val >= 0:
                     # print('North is a valid node')
-                    holding.append([x_val,y_val])
+                    holding.append([x_val, y_val])
 
         except IndexError:
             pass
             # print("North is out of range")
-
-
-         #Check South
+         # Check South
 
         x_val = current_visiting_node[0]
         y_val = current_visiting_node[1] + 1
@@ -157,19 +153,17 @@ def weighted_search(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,siz
         except IndexError:
             pass
             # print("South is out of range")
-
-
-        #Check East
+        # Check East
 
         x_val = current_visiting_node[0]+1
-        y_val = current_visiting_node[1] 
+        y_val = current_visiting_node[1]
 
         # print("checking East:")
         # print("Current Visitng node", x_val, ",", y_val, " ", visited)
 
         try:
             if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
-                if x_val<= size_x-1:
+                if x_val <= size_x-1:
                     # print('East is a valid node')
                     holding.append([x_val, y_val])
 
@@ -177,10 +171,10 @@ def weighted_search(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,siz
             # print("East is out of range")
             pass
 
-        #Check WEst
+        # Check WEst
 
-        x_val = current_visiting_node[0] -1
-        y_val = current_visiting_node[1] 
+        x_val = current_visiting_node[0] - 1
+        y_val = current_visiting_node[1]
 
         # print("checking West:")
         # print("Current Visitng node", x_val, ",", y_val, " ", visited)
@@ -195,7 +189,7 @@ def weighted_search(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,siz
         except IndexError:
             pass
             # print("West is out of range")
-        
+
         if holding != []:
             # print("We have found the following visitable nodes around",current_visiting_node)
             # print("Visitable nodes",holding)
@@ -208,55 +202,46 @@ def weighted_search(table,coords,start_x,start_y,end_x,end_y,node_map,size_x,siz
 
                 # print('weight map')
                 print(weight_map)
-                
-                #Use dictonary to find ideal weight
-           
-            minVal = (min(weight_map, key= weight_map.get))
 
-            if  weight_map[minVal] == 0.0:
+                # Use dictonary to find ideal weight
+
+            minVal = (min(weight_map, key=weight_map.get))
+
+            if weight_map[minVal] == 0.0:
                 # print("shortest found")
                 del visited[0]
                 return visited
-
 
             # print("best node to visit is ", minVal)
 
             clean_list = json.loads(minVal)
             clean_list_x = clean_list[0]
             clean_list_y = clean_list[1]
-            table[clean_list_y][clean_list_x]= '#'
+            table[clean_list_y][clean_list_x] = '#'
 
             visited.append(clean_list)
 
-            
-        
         else:
             print("There are no visitable nodes ")
             print("Fixing")
             table[y_val][x_val] = "#"
 
-            if len(visited) > size_x * size_y +1:
+            if len(visited) > size_x * size_y + 1:
                 return False
 
             del visited[-1]
             current_visiting_node = visited[-1]
 
-            
-
-            #Check north
+            # Check north
 
             x_val = current_visiting_node[0]
             y_val = current_visiting_node[1]
             table[y_val][x_val] = "."
 
-            
-
-
 
 
         # stack.append
         # cont = input("")
-
 
 '''
 node_dictionary = 
@@ -270,22 +255,22 @@ node_dictionary =
 }
 '''
 
-def genNodeMap(table,end_x,end_y):
+
+def genNodeMap(table, end_x, end_y):
 
     node_map = {}
 
     for i in range(len(table)):
         for k in range(len(table[i])):
             end_distance = math.sqrt(((end_x - k)**2 + (end_y - i)**2))
-            node_map['['+ str(k)+', '+str(i)+']'] = {"value":str(table[i][k]),"distance_from_start":999, "from":"","distance_to_end": round(end_distance,3),"shortest_found":False}
-    
-    #print(node_map)
-    
+            node_map['[' + str(k)+', '+str(i)+']'] = {"value": str(table[i][k]), "distance_from_start": 999,
+                                                      "from": "", "distance_to_end": round(end_distance, 3), "shortest_found": False}
+
+    # print(node_map)
+
     return node_map
 
-
-
-def foundEnd(coord,end_x,end_y):
+def foundEnd(coord, end_x, end_y):
 
    # print("Debug, printing Coord",coord)
     k = coord[-1][0]
@@ -296,32 +281,30 @@ def foundEnd(coord,end_x,end_y):
 
     if end_distance == 1 or end_distance == 1.0:
         return True
-    
+
     else:
         return False
 
-
-
-def check_valid_north(table,coordinate,size_x,size_y):
+def check_valid_north(table, coordinate, size_x, size_y):
 
     x_val = coordinate[0]
     y_val = coordinate[1] - 1
 
     try:
-            if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
-                if y_val >= 0:
-                    # print('North is a valid node')
+        if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
+            if y_val >= 0:
+                # print('North is a valid node')
 
-                     return [x_val, y_val]
+                return [x_val, y_val]
 
-                else:
-                   # print('North is NOT a valid node')
+            else:
+               # print('North is NOT a valid node')
 
-                    return False
+                return False
 
     except IndexError:
-           # print("North is out of range")
-            return False
+        # print("North is out of range")
+        return False
 
 
 def check_valid_south(table, coordinate, size_x, size_y):
@@ -329,84 +312,81 @@ def check_valid_south(table, coordinate, size_x, size_y):
     y_val = coordinate[1] + 1
 
     try:
-            if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
-                if y_val <= size_y-1:
-                     #print('South is a valid node')
+        if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
+            if y_val <= size_y-1:
+                #print('South is a valid node')
 
-                     return [x_val, y_val]
+                return [x_val, y_val]
 
-                else:
-                    print('South is NOT a valid node')
+            else:
+                print('South is NOT a valid node')
 
-                    return False
+                return False
 
     except IndexError:
-           # print("South is out of range")
-            return False
-    
+        # print("South is out of range")
+        return False
+
 
 def check_valid_east(table, coordinate, size_x, size_y):
     x_val = coordinate[0]+1
-    y_val = coordinate[1] 
-
-    try:
-            if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
-                if x_val <= size_x - 1:
-                     #print('east is a valid node')
-
-                     return [x_val, y_val]
-
-                else:
-                   # print('east is NOT a valid node')
-
-                    return False
-
-    except IndexError:
-            #print("east is out of range")
-
-            return False
-
-
-def check_valid_west(table, coordinate, size_x, size_y):
-    x_val = coordinate[0] -1
     y_val = coordinate[1]
 
     try:
-            if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
-                if x_val >= 0:
-                    # print('west is a valid node')
+        if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
+            if x_val <= size_x - 1:
+                #print('east is a valid node')
 
-                     return [x_val, y_val]
+                return [x_val, y_val]
 
-                else:
-                   # print('west is NOT a valid node')
+            else:
+               # print('east is NOT a valid node')
 
-                    return False
+                return False
 
     except IndexError:
-            #print("west is out of range")
-            return False
+        #print("east is out of range")
+
+        return False
 
 
+def check_valid_west(table, coordinate, size_x, size_y):
+    x_val = coordinate[0] - 1
+    y_val = coordinate[1]
 
-def valid(action,table,deque,size_x,size_y):
+    try:
+        if table[y_val][x_val] == "." or table[y_val][x_val] == "X":
+            if x_val >= 0:
+                # print('west is a valid node')
 
+                return [x_val, y_val]
+
+            else:
+               # print('west is NOT a valid node')
+
+                return False
+
+    except IndexError:
+        #print("west is out of range")
+        return False
+
+
+def valid(action, table, deque, size_x, size_y):
 
 
     if action == "N":
-        return check_valid_north(table,deque,size_x,size_y)
+        return check_valid_north(table, deque, size_x, size_y)
 
     elif action == "S":
         return check_valid_south(table, deque, size_x, size_y)
-    
+
     elif action == "E":
         return check_valid_east(table, deque, size_x, size_y)
-    
+
     elif action == "W":
         return check_valid_west(table, deque, size_x, size_y)
 
-    
-    
+
 def vibrant_table(table):
     for i in range(len(table)):
         for k in range(len(table)):
@@ -424,60 +404,46 @@ def vibrant_table(table):
             if table[k][i] == "O":
                 table[k][i] = "⛹"
 
-    return table 
+    return table
 
 
-   
-
-
-
-
-def bfs(coords,table, start_x,start_y,end_x,end_y,size_x,size_y):
+def bfs(coords, table, start_x, start_y, end_x, end_y, size_x, size_y):
 
     table = copy.deepcopy(table)
     pretty_table = copy.deepcopy(table)
 
     pretty_table = vibrant_table(pretty_table)
-        
-
-                
 
     print_table(pretty_table)
     input("")
     queue = []
     action_list = []
-    queue.append([[start_x,start_y]])
+    queue.append([[start_x, start_y]])
     dequeued = queue[0]
 
-    
-
-    
-    
     #last_queue_coord = queue[0][-1]
 
-    while foundEnd(dequeued,end_x,end_y) is False:
+    while foundEnd(dequeued, end_x, end_y) is False:
         #print("Queue: ", queue)
         #print("Distance to end", foundEnd(dequeued, end_x, end_y))
         dequeued = queue[0]
+        # TODO when on second round, it keeps checking first value of queue
 
-
-        #TODO when on second round, it keeps checking first value of queue
-
-       # print("Dequed Sequence: ", dequeued)
+        # print("Dequed Sequence: ", dequeued)
         del queue[0]
         #print("Queue remaining: ", queue)
 
         last_coord_of_dequed = dequeued[-1]
 
-        for actions in ["N","S","E","W"]:
-           
+        for actions in ["N", "S", "E", "W"]:
 
            # print("in for loop, printing dequed and then 'dequed[-1]' ",dequeued)
            # print(dequeued[-1])
 
-            check_actions = valid(actions, table, last_coord_of_dequed,size_x,size_y)
+            check_actions = valid(
+                actions, table, last_coord_of_dequed, size_x, size_y)
 
-            #TODO check logic of adding new value to dequed or queue
+            # TODO check logic of adding new value to dequed or queue
 
             if check_actions is False or check_actions is None:
                # print(actions,"is not viable")
@@ -486,9 +452,9 @@ def bfs(coords,table, start_x,start_y,end_x,end_y,size_x,size_y):
             dequeued_copy = copy.deepcopy(dequeued)
             dequeued_copy.append(check_actions)
 
-            #print(add_to_dequeued)
+            # print(add_to_dequeued)
 
-            #TODO problem appears to be here
+            # TODO problem appears to be here
             queue.append(dequeued_copy)
            # print("The Queue", queue)
 
@@ -497,58 +463,48 @@ def bfs(coords,table, start_x,start_y,end_x,end_y,size_x,size_y):
             table[check_actions[1]][check_actions[0]] = "#"
             pretty_table[check_actions[1]][check_actions[0]] = "✔"
             os.system('cls')
-            #print('==============================================')
-            #time.sleep(0.001)
+            # print('==============================================')
+            # time.sleep(0.001)
             print_table(pretty_table)
-    
+
     print("FOUND END")
     print(dequeued)
 
     return dequeued
+    # if  is True:
 
 
-            #if  is True:
-
-            
-
-
-
-    
-
-
-
-
-def path_table(visits,obstacle_table):
+def path_table(visits, obstacle_table):
     print(visits)
 
     for node in visits:
 
         obstacle_table[node[1]][node[0]] = "✔️"
     obstacle_table = vibrant_table(obstacle_table)
-        
+
     return obstacle_table
-
-
 
 
 
 if __name__ == "__main__":
     while True:
-        x,y = setSize()
-        empty_table = construct_table(x,y)
+        x, y = setSize()
+        empty_table = construct_table(x, y)
         # print_table(empty_table)
-        start_point_table, start_x,start_y,end_x,end_y = startPoint(empty_table,x,y)
+        start_point_table, start_x, start_y, end_x, end_y = startPoint(
+            empty_table, x, y)
 
-        #print_table(start_point_table)
-        obstacle_table = obstacle(start_point_table,x , y)
+        # print_table(start_point_table)
+        obstacle_table = obstacle(start_point_table, x, y)
 
         print_table(obstacle_table)
 
-        coords = genCoordinates(obstacle_table,start_x,start_y,end_x,end_y)
-        node_map = genNodeMap(obstacle_table,end_x,end_y)
+        coords = genCoordinates(obstacle_table, start_x, start_y, end_x, end_y)
+        node_map = genNodeMap(obstacle_table, end_x, end_y)
 
         weighted_search_result_table = copy.deepcopy(obstacle_table)
-        weighted_search_result = weighted_search(weighted_search_result_table, coords, start_x, start_y, end_x, end_y, node_map, x, y)
+        weighted_search_result = weighted_search(
+            weighted_search_result_table, coords, start_x, start_y, end_x, end_y, node_map, x, y)
 
         if weighted_search_result is False:
             print('\nNo Solution')
@@ -562,34 +518,28 @@ if __name__ == "__main__":
 
         """)
 
-        print_table( path_table(weighted_search_result,weighted_search_result_table))
+        print_table(path_table(weighted_search_result,
+                               weighted_search_result_table))
 
         print(weighted_search_result)
         print("Distance is ", len(weighted_search_result))
 
-
         cont = input("")
-
 
         print("==============BREATH FIRST SEARCH==========")
 
         bfs_table = copy.deepcopy(obstacle_table)
 
-        bfs_result = bfs(coords,bfs_table,start_x,start_y,end_x,end_y,x,y)
+        bfs_result = bfs(coords, bfs_table, start_x,
+                         start_y, end_x, end_y, x, y)
 
         bfs_result = bfs_result[1:]
-
 
 
         print_table(path_table(bfs_result, bfs_table))
         print(bfs_result)
         print("Distance is ", len(bfs_result))
-
-
-   
-        
-
-    #print(coords)
+    # print(coords)
 
     # while True:
     #     temp = input("")
