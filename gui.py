902e27b0e1b4ea89fd26068c6f6adc4314451d84
AyaanGH <25395404+AyaanGH@users.noreply.github.com>
2020-04-07 11:58:00 +0100

    Added drag motion

--- gui.py
+++ gui.py
@@ -1,16 +1,25 @@
 import tkinter as tk
+import random
 
 
 root = tk.Tk()
+sidebar = tk.Frame(root,width=200,height=500, borderwidth = 2)
 
+sidebar.pack(expand=False,fill='both',side = 'right', anchor = 'nw')
 
 
 class Cell:
 
     def  __init__(self,canvas,x,y,size):
         self.canvas = canvas
-        self.box = self.canvas.create_rectangle(x,y,x+size,y+size)
+        self.box = self.canvas.create_rectangle(x,y,x+size,y+size, fill = "white")
+        # self.canvas.tag_bind(self.box,"<Motion><B1-Motion>",lambda x: self.set_colour("pink"))
         self.colour = ""
+        self.dragging = False
+        self.canvas.bind("<ButtonPress-1>", self.on_click)
+        self.canvas.bind("<B1-Motion>", self.on_move)
+        self.canvas.bind("<ButtonRelease-1>", self.on_release)
+
         
     def set_colour(self,colour):
         self.canvas.itemconfigure(self.box,fill=colour)
@@ -18,10 +27,24 @@ class Cell:
     def get_colour(self):
         self.canvas.itemcget(self.box,"fill")
 
+    
+    def on_click(self, event):
+            self.dragging = True
+            self.on_move(event)
+
+    def on_move(self, event):
+            if self.dragging:
+                items = self.canvas.find_closest(event.x, event.y)
+                if items:
+                    rect_id = items[0]
+                    self.canvas.itemconfigure(rect_id, fill="pink")
+
+    def on_release(self, event):
+            self.dragging = False
 
 
 
-root.resizable(False,False)
+# root.resizable(False,False)
 
 w = tk.Canvas(root, width = 500, height = 500)
 
@@ -52,6 +75,20 @@ for y in range(0,height, square_size):
 
 
 grid[0][10].set_colour('red')
+
+
+grid[5][20].set_colour('green')
+
+
+
+head = grid[5][20]
+
+
+
+
+
+
+
 
 w.pack()
 
